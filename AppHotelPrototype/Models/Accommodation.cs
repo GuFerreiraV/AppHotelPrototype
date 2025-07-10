55376afdb6618908d16bdc129a319c86137c666d
GuFerreiraV <gustavoferreiravargens@gmail.com>
2025-07-10 17:48:04 -0300

    Criação da model Accommotadtion

---
+++ AppHotelPrototype/Models/Accommodation.cs
@@ -0,0 +1,30 @@
+﻿namespace AppHotelPrototype.Models
+{
+    public class Accommodation
+    {
+
+        public Room SelectedRoom { get; set; }
+
+        public int QtdeAdults { get; set; }
+
+        public int QtdeChildrens { get; set; }
+
+        public DateTime CheckinDate { get; set; }
+        public DateTime CheckoutDate { get; set; }
+
+        // Estadia
+        public int Stay
+        {
+            get => CheckoutDate.Subtract(CheckinDate).Days;
+
+        }
+
+        public double TotalValue
+        {
+            get => (QtdeAdults * SelectedRoom.PriceAdult) + (QtdeChildrens * SelectedRoom.PriceChild);
+
+        }
+
+
+    }
+}
